ee8393a61666acc06a7a0efef4eba3f8bda94b14
Krishna Vamsi Pinninti <krishpinninti789@gmail.com>
2026-09-16 21:23:12 +0530

    fixed component and styles

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,10 +1,11 @@
 import { MonitorPlay } from "lucide-react";
 import React from "react";
-import Search from "./Search";
+import Search from "./SearchBar";
+import SearchBar from "./SearchBar";
 
 const Header = () => {
   return (
-    <div className="w-full sticky top-0 flex flex-row gap-x-64 px-8 py-4 border border-b-gray-800">
+    <div className="w-full sticky top-0 flex flex-row gap-x-64 px-8 py-4 border-b border-gray-800">
       <div className="flex gap-x-2 justify-center items-center">
         <MonitorPlay className="text-yellow-800 w-8 h-8" />
         <h1 className="text-3xl text-yellow-800">
@@ -13,7 +14,7 @@ const Header = () => {
         </h1>
       </div>
 
-      <Search />
+      <SearchBar />
     </div>
   );
 };
---
+++ app/components/SearchBar.tsx
@@ -0,0 +1,26 @@
+import { Search } from "lucide-react";
+import React from "react";
+
+const SearchBar = () => {
+  return (
+    <div
+      className="flex items-center justify-center p-3 rounded-2xl border border-gray-500 focus:border-yellow-700  w-3xl focus:ring-2 focus:ring-yellow-800 bg-gray-900 placeholder:text-yellow-900 text-yellow-700 
+        focus-within:ring-2
+        focus-within:ring-yellow-700  transition-all
+    duration-300
+    ease-in-out"
+    >
+      <input
+        placeholder="Search"
+        className="w-3xl bg-gray-900 placeholder:text-yellow-900 text-yellow-700 focus:outline-none  transition-all
+    duration-300
+    ease-in-out"
+      />
+      <button className="cursor-pointer">
+        <Search />
+      </button>
+    </div>
+  );
+};
+
+export default SearchBar;

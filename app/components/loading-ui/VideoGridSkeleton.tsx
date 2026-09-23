0b1390cfdbe0c743d13f9f07444d356509cb3656
Krishna Vamsi Pinninti <krishpinninti789@gmail.com>
2026-09-23 22:57:21 +0530

    redesign

--- app/components/loading-ui/VideoGridSkeleton.tsx
+++ app/components/loading-ui/VideoGridSkeleton.tsx
@@ -2,7 +2,7 @@ import VideoCardSkeleton from "./VideoCardSkeleton";
 
 const VideoGridSkeleton = () => {
   return (
-    <div className="grid grid-cols-3 gap-6 px-8 py-4">
+    <div className="grid md:grid-cols-3 gap-6 px-8 py-4">
       {Array.from({ length: 12 }).map((_, index) => (
         <VideoCardSkeleton key={index} />
       ))}
